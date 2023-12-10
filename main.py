8e2a7d978e832767a43c60446a07c1064ccd05f3
stepanshustov <step.shustov@mail.ru>
2023-12-10 20:22:05 +0400

    персонаж прыгает адекватно

--- main.py
+++ main.py
@@ -8,32 +8,55 @@ def load_image(name):
     return image
 
 
+class Pers(pygame.sprite.Sprite):
+    image = load_image("pers.png")
+
+    def __init__(self, *group):
+        super().__init__(*group)
+        self.image = load_image("pers.png")
+        self.image = pygame.transform.scale(self.image, (150, 150))
+        self.rect = self.image.get_rect()
+        self.rect.x = 100
+        self.rect.y = 100
+
+
 if __name__ == '__main__':
     pygame.init()
     size = width, height = 1280, 720
     screen = pygame.display.set_mode(size)
     running = True
-    fon = load_image("fon.jpg")
-    x = y = 0
+    pers = Pers()
+    x = y = 500
     clock = pygame.time.Clock()
+    all_sprites = pygame.sprite.Group()
+    all_sprites.add(pers)
+    k = 0
+    flag = 0
+    tm = 0
+    s = 0
+    y0 = y1 = y
+    a = (457, 652)
+    b = (657, 652)
     while running:
-        clock.tick(60)
+        clock.tick(50)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
-        pr = pygame.key.get_pressed()
-
-        if pr[pygame.K_LEFT]:
-            x -= 2
-        if pr[pygame.K_RIGHT]:
-            x += 2
-        if pr[pygame.K_UP]:
-            y -= 2
-        if pr[pygame.K_DOWN]:
-            y += 2
-        x %= width
-        y %= height
-        screen.fill((0, 0, 0))
-        screen.blit(fon, (x - width, y - height))
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and flag < 2:
+                    if flag == 0:
+                        y0 = y
+                    flag += 1
+                    k = 8
+        if y - k < y0 and flag:
+            y -= k
+            k -= 0.3
+        else:
+            y = y0
+            flag = 0
+        screen.fill((0, 50, 0))
+        pygame.draw.line(screen, "red", a, b, 4)
+        pers.rect.x = x
+        pers.rect.y = y
+        all_sprites.draw(screen)
         pygame.display.flip()
